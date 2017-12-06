b7506b05afea31ec7282aa7d20cb691870a63182
Venreaver <reaversindustry@gmail.com>
2017-12-06 17:07:14 +0300

    workers improvement

--- ConcurrencyDec2017/src/lesson171205/UseWorker.java
+++ ConcurrencyDec2017/src/lesson171205/UseWorker.java
@@ -3,32 +3,30 @@ package lesson171205;
 import lesson171201.Utils;
 
 public class UseWorker {
-	
-	public static void main(String[] args) {
-		
-		System.out.println("main started");
-		
-		Worker worker = new Worker();
-		
-		worker.execute(() -> {
-			Utils.pause(1000);
-			System.out.println("one");
-		});
 
-		worker.execute(() -> {
-			Utils.pause(1000);
-			System.out.println("two");
-		});
+    public static void main(String[] args) {
+
+        System.out.println("main started");
+
+        Worker worker = new Worker();
+
+        worker.execute(() -> {
+            Utils.pause(1000);
+            System.out.println("one");
+        });
+
+        worker.execute(() -> {
+            Utils.pause(1000);
+            System.out.println("two");
+        });
 
         worker.shutDown();
-		
-		worker.execute(() -> {
-			Utils.pause(1000);
-			System.out.println("three");
-		});
 
-		System.out.println("main finished");
+        worker.execute(() -> {
+            Utils.pause(1000);
+            System.out.println("three");
+        });
 
-	}
-
+        System.out.println("main finished");
+    }
 }
--- ConcurrencyDec2017/src/lesson171205/Worker.java
+++ ConcurrencyDec2017/src/lesson171205/Worker.java
@@ -1,50 +1,31 @@
 package lesson171205;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.concurrent.Executor;
 
 public class Worker implements Executor {
+    private MyBlockingQueue<Runnable> tasks = new MyBlockingQueue<>();
+    private static final Runnable POISON_PILL = () -> {
+    };
 
-	private final Object mutex = new Object();
-	private Queue<Runnable> tasks = new LinkedList<>();
-	private volatile boolean isStopped;
+    public Worker() {
+        new Thread(this::processTasks).start();
+    }
 
-	public Worker() {
-		new Thread(this::processTasks).start();
-	}
+    public void execute(Runnable task) {
+        tasks.put(task);
+    }
 
-	public void execute(Runnable task) {
-		synchronized (mutex) {
-		    if (!isStopped) {
-                tasks.offer(task);
-                mutex.notify();
+    public void shutDown() {
+        tasks.put(POISON_PILL);
+    }
+
+    private void processTasks() {
+        while (true) {
+            Runnable task = tasks.take();
+            if (task == POISON_PILL) {
+                break;
             }
+            task.run();
         }
-	}
-
-	public void shutDown() {
-        isStopped = true;
-	}
-
-	private void processTasks() {
-		while (true) {
-			Runnable task = null;
-			synchronized (mutex) {
-				while (tasks.isEmpty()) {
-					if (isStopped) {
-						return;
-					}
-					try {
-						mutex.wait();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-				task = tasks.poll();
-			}
-			task.run();
-		}
-	}
-
+    }
 }
